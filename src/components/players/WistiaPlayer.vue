37735d519f5f4e0f741b8043a6ca38b03aecf56c
Voltra <ludwig.guerin.98@gmx.fr>
2022-04-09 15:16:31 +0200

    Add Wistia player

---
+++ src/components/players/WistiaPlayer.vue
@@ -0,0 +1,188 @@
+<template>
+	//
+</template>
+
+<script>
+	import { randomString } from "../../utils";
+	import { canPlay } from "../../patterns";
+	import { playerMixin } from "../../mixins/player";
+	import { wistiaConfigProps } from "../../props/wistiaConfig";
+
+	const SDK_URL = "https://fast.wistia.com/assets/external/E-v1.js";
+	const SDK_GLOBAL = "Wistia";
+	const PLAYER_ID_PREFIX = "wistia-player-";
+
+	export default {
+		// [META]
+		displayName: "Wistia",
+		canPlay: canPlay.wistia,
+		loopOnEnded: true,
+		// [/META]
+
+		mixins: [playerMixin],
+
+		props: {
+			config: wistiaConfigProps,
+		},
+
+		data() {
+			return {
+				randomId: `${PLAYER_ID_PREFIX}${randomString()}`,
+			};
+		},
+
+		computed: {
+			pid() {
+				return this.config.playerId ?? this.randomId;
+			},
+		},
+		methods: {
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook play
+			 */
+			play() {
+				this.callPlayer("play");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook pause
+			 */
+			pause() {
+				this.callPlayer("pause");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook stop
+			 */
+			stop() {
+				this.unbind();
+				this.callPlayer("remove");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook seekTo
+			 */
+			seekTo(seconds) {
+				this.callPlayer("time", seconds);
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook setVolume
+			 */
+			setVolume(fraction) {
+				this.callPlayer("volume", fraction);
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook mute
+			 */
+			mute() {
+				this.callPlayer("mute");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook unmute
+			 */
+			unmute() {
+				this.callPlayer("unmute");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook setPlaybackRate
+			 */
+			setPlaybackRate(rate) {
+				this.callPlayer("playbackRate", rate);
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook setPlaybackRate
+			 */
+			getDuration() {
+				return this.callPlayer("duration");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook getCurrentTime
+			 */
+			getCurrentTime() {
+				return this.callPlayer("time");
+			},
+
+			/**
+			 * @inheritDoc
+			 * @override
+			 * @hook getSecondsLoaded
+			 */
+			getSecondsLoaded() {
+				return null;
+			},
+
+			async load(url) {
+				try {
+					const { playing, muted, controls, onReady, config, onError } = this.props;
+
+					const Wistia = await getSDK(SDK_URL, SDK_GLOBAL);
+					config.customControls?.forEach?.(control => Wistia.defineControl(control));
+					window._wq = window._wq || [];
+					window._wq.push({
+						id: this.playerID,
+						options: {
+							autoPlay: playing,
+							silentAutoPlay: "allow",
+							muted: muted,
+							controlsVisibleOnLoad: controls,
+							fullscreenButton: controls,
+							playbar: controls,
+							playbackRateControl: controls,
+							qualityControl: controls,
+							volumeControl: controls,
+							settingsControl: controls,
+							smallPlayButton: controls,
+							...config.options,
+						},
+						onReady: player => {
+							this.player = player;
+							this.unbind();
+							this.player.bind("play", this.onPlay);
+							this.player.bind("pause", this.onPause);
+							this.player.bind("seek", this.onSeek);
+							this.player.bind("end", this.onEnded);
+							this.player.bind("playbackratechange", this.onPlaybackRateChange);
+							onReady();
+						},
+					});
+				} catch (e) {
+					this.onError(e);
+				}
+			},
+
+			unbind() {
+				this.player?.unbind?.("play", this.onPlay);
+				this.player?.unbind?.("pause", this.onPause);
+				this.player?.unbind?.("seek", this.onSeek);
+				this.player?.unbind?.("end", this.onEnded);
+				this.player?.unbind?.("playbackratechange", this.onPlaybackRateChange);
+			},
+		},
+	};
+</script>
