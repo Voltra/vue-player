3d2925df5f283bbefd208dd7947d35a81599abeb
Voltra <ludwig.guerin.98@gmx.fr>
2024-02-09 18:25:42 +0100

    -> v0.0.5

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 export * from "./mixins/player";
 
 // Meta player
-export { default as VuePlayer } from "@/components/VuePlayer"
+export { default as VuePlayer } from "./components/VuePlayer"
 
 // Helpers
-export { default as Player } from "@/components/Player"
-export { default as Preview } from "@/components/Preview"
+export { default as Player } from "./components/Player"
+export { default as Preview } from "./components/Preview"
 
 // Players
-export { default as DailyMotionPlayer } from "@/components/players/DailyMotionPlayer"
-export { default as FacebookPlayer } from "@/components/players/FacebookPlayer"
-export { default as FilePlayer } from "@/components/players/FilePlayer"
-export { default as KalturaPlayer } from "@/components/players/KalturaPlayer"
-export { default as MixcloudPlayer } from "@/components/players/MixcloudPlayer"
-export { default as SoundcloudPlayer } from "@/components/players/SoundcloudPlayer"
-export { default as StreamablePlayer } from "@/components/players/StreamablePlayer"
-export { default as TwitchPlayer } from "@/components/players/TwitchPlayer"
-export { default as VidyardPlayer } from "@/components/players/VidyardPlayer"
-export { default as VimeoPlayer } from "@/components/players/VimeoPlayer"
-export { default as WistiaPlayer } from "@/components/players/WistiaPlayer"
-export { default as YouTubePlayer } from "@/components/players/YouTubePlayer"
+export { default as DailyMotionPlayer } from "./components/players/DailyMotionPlayer"
+export { default as FacebookPlayer } from "./components/players/FacebookPlayer"
+export { default as FilePlayer } from "./components/players/FilePlayer"
+export { default as KalturaPlayer } from "./components/players/KalturaPlayer"
+export { default as MixcloudPlayer } from "./components/players/MixcloudPlayer"
+export { default as SoundcloudPlayer } from "./components/players/SoundcloudPlayer"
+export { default as StreamablePlayer } from "./components/players/StreamablePlayer"
+export { default as TwitchPlayer } from "./components/players/TwitchPlayer"
+export { default as VidyardPlayer } from "./components/players/VidyardPlayer"
+export { default as VimeoPlayer } from "./components/players/VimeoPlayer"
+export { default as WistiaPlayer } from "./components/players/WistiaPlayer"
+export { default as YouTubePlayer } from "./components/players/YouTubePlayer"
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,24 @@
-declare module "@voltra/vue-player" {}
+declare module "@voltra/vue-player" {
+	export * from "./mixins/player";
 
-export type * from "./patterns";
-export type * from "./players";
-export type * from "./utils";
+	// Meta player
+	export { default as VuePlayer } from "./components/VuePlayer"
+
+	// Helpers
+	export { default as Player } from "./components/Player"
+	export { default as Preview } from "./components/Preview"
+
+	// Players
+	export { default as DailyMotionPlayer } from "./components/players/DailyMotionPlayer"
+	export { default as FacebookPlayer } from "./components/players/FacebookPlayer"
+	export { default as FilePlayer } from "./components/players/FilePlayer"
+	export { default as KalturaPlayer } from "./components/players/KalturaPlayer"
+	export { default as MixcloudPlayer } from "./components/players/MixcloudPlayer"
+	export { default as SoundcloudPlayer } from "./components/players/SoundcloudPlayer"
+	export { default as StreamablePlayer } from "./components/players/StreamablePlayer"
+	export { default as TwitchPlayer } from "./components/players/TwitchPlayer"
+	export { default as VidyardPlayer } from "./components/players/VidyardPlayer"
+	export { default as VimeoPlayer } from "./components/players/VimeoPlayer"
+	export { default as WistiaPlayer } from "./components/players/WistiaPlayer"
+	export { default as YouTubePlayer } from "./components/players/YouTubePlayer"
+}
