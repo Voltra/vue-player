60ea26f3b145de290de52acbd7f03fe0ee8434f4
Voltra <ludwig.guerin.98@gmx.fr>
2024-09-17 22:23:42 +0200

    Reset to a clean base for v1.0.0

--- dist/cjs/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("./index-d4040e53.js");exports.VuePlayer=e.script,exports.playerMixin=e.playerMixin;
-//# sourceMappingURL=index.js.map
--- dist/esm/index.js
+++
@@ -1,2 +0,0 @@
-export{U as VuePlayer,p as playerMixin}from"./index-15f1a121.js";
-//# sourceMappingURL=index.js.map
